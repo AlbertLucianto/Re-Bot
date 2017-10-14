b17e348ec0cfafc0f1162d8e259d7df072748799
Albert Lucianto <albertlucianto@gmail.com>
2017-10-15 03:40:51 +0800

    add interactions

---
+++ client/src/App.vue
@@ -0,0 +1,90 @@
+<template>
+  <div id="app">
+    <div class="auto-complete" :class="{ hidden: activeIdx >= 0 }">
+      <input class="text-input" placeholder="Search security"></input>
+    </div>
+    <card :open="activeIdx === idx"
+      :toggleActive="toggleActive"
+      v-for="(name, idx) in cards"
+      :key="idx"
+      :idx="idx"
+      :name="name"
+      :hidden="activeIdx !== idx && activeIdx >= 0"></card>
+  </div>
+</template>
+
+<script>
+import Card from '@/components/Card';
+
+export default {
+  name: 'app',
+  components: {
+    Card,
+  },
+  data() {
+    return {
+      activeIdx: 0,
+      cards: [
+        'AAPL',
+        'GOOGL',
+        'MSFT',
+      ],
+    };
+  },
+  methods: {
+    toggleActive(num) {
+      this.activeIdx = num;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import './components/styles/colors.scss';
+
+body {
+  margin: 0;
+}
+#app {
+  background: $white;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+textarea, input {
+  border: 0;
+}
+.auto-complete {
+  width: 100vw;
+  height: 150px;
+  background: $pearl;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-height: 200px;
+  transition: all .5s ease .5s;
+  overflow: hidden;
+  .text-input {
+    width: calc(100vw - 120px);
+    height: 100px;
+    background: $lavender;
+    outline: 0;
+    border-radius: 20px;
+    box-shadow: 0 10px 40px rgba(0,0,0,.15);
+    padding: 2px 30px 8px 30px;
+    font-size: 50px;
+    color: $imperial;
+    text-align: center;
+    &::-webkit-input-placeholder {
+      color: $pearl;
+      opacity: .6;
+    }
+  }
+  &.hidden {
+    max-height: 0;
+    transition: all .1s ease;
+  }
+}
+</style>
---
+++ client/src/components/Card.vue
@@ -0,0 +1,239 @@
+<template>
+  <div class="card" :style="cardStyle" :class="{ inactive: !open, hidden }" @mousedown="openThis">
+    <h1 class="security-name" :class="{ inactive: !open }">{{ name }}</h1>
+    <div class="rim-out" v-if="open" ref="rimOut"
+      @mousedown="startScroll" @mousemove="doScroll" @mouseup="stopScroll">
+      <div class="rim-in" @mousedown="stopPropagate">
+        <div class="data">
+          <div class="label">Total</div>
+          <div class="value">{{ total }}</div>
+        </div>
+      </div>
+    </div>
+    <div class="swipe-container" @mousedown="startDrag"
+      @mousemove="startMove" @mouseup="stopDrag" v-if="open">
+      <svg class="swipe-up">
+        <g class="line-group" v-for="arrow in arrows" :key="arrow">
+          <line x1="-40" :y1="30 + (arrow * 40)" x2="0" :y2="arrow * 40" class="swipe-line"></line>
+          <line x1="40" :y1="30 + (arrow * 40)" x2="0" :y2="arrow * 40" class="swipe-line"></line>
+        </g>
+      </svg>
+      <div class="swipe-text">see all your securities</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import dynamics from 'dynamics.js';
+
+const scrollConstant = 0.05;
+
+export default {
+  data() {
+    return {
+      arrows: [0, 1, 2],
+      dragOff: 0,
+      startPos: 0,
+      dragging: false,
+      total: 15,
+      scrolling: false,
+      scrollBuffer: 0,
+      lastScrollPos: { x: 0, y: 0 },
+    };
+  },
+  props: {
+    toggleActive: Function,
+    open: Boolean,
+    idx: Number,
+    name: String,
+    hidden: Boolean,
+  },
+  computed: {
+    cardStyle() {
+      return {
+        transform: `translate(0, ${this.dragOff / 3}px) scale(${(2000 + this.dragOff) / 2000})`,
+        'border-radius': `${Math.min(Math.abs(this.dragOff / 10), 50)}px`,
+      };
+    },
+  },
+  methods: {
+    startDrag(e) {
+      this.dragging = true;
+      const evt = e.changedTouches ? e.changedTouches[0] : e;
+      this.startPos = evt.pageY;
+      window.addEventListener('mousemove', this.startMove);
+      window.addEventListener('mouseup', this.stopDrag);
+    },
+    startMove(e) {
+      if (this.dragging) {
+        const evt = e.changedTouches ? e.changedTouches[0] : e;
+        const move = evt.pageY - this.startPos;
+        this.dragOff = move < 0 ? move : 0;
+      }
+    },
+    stopDrag() {
+      window.removeEventListener('mousemove', this.startMove);
+      window.removeEventListener('mouseup', this.stopDrag);
+      this.dragging = false;
+      if (this.open && Math.abs(this.dragOff) > 500) {
+        this.toggleActive(-1);
+      }
+      dynamics.animate(this, {
+        dragOff: 0,
+      }, {
+        type: dynamics.easeOut,
+        duration: 500,
+        friction: 50,
+      });
+    },
+    openThis() {
+      this.toggleActive(this.idx);
+    },
+    startScroll(e) {
+      this.scrolling = true;
+      const evt = e.changedTouches ? e.changedTouches[0] : e;
+      this.lastScrollPos = {
+        x: evt.pageX,
+        y: evt.pageY,
+      };
+    },
+    doScroll(e) {
+      if (this.scrolling) {
+        const evt = e.changedTouches ? e.changedTouches[0] : e;
+        const rect = this.$refs.rimOut.getBoundingClientRect();
+        const rimCenter = {
+          x: (rect.left + rect.right) / 2,
+          y: (rect.top + rect.bottom) / 2,
+        };
+        const dx = evt.pageX - this.lastScrollPos.x;
+        const dy = evt.pageY - this.lastScrollPos.y;
+        const vector = {
+          x: -(this.lastScrollPos.y - rimCenter.y),
+          y: this.lastScrollPos.x - rimCenter.x,
+        };
+        const vectorLength = Math.sqrt((vector.x ** 2) + (vector.y ** 2));
+        const unitVector = {
+          x: vector.x / vectorLength,
+          y: vector.y / vectorLength,
+        };
+        this.scrollBuffer += ((unitVector.x * dx) + (unitVector.y * dy)) * scrollConstant;
+        if (this.scrollBuffer > 1 || this.scrollBuffer < 0) {
+          const change = Math.floor(this.scrollBuffer);
+          this.total = Math.max(this.total + change, 0);
+          this.scrollBuffer -= change;
+        }
+        this.lastScrollPos = {
+          x: evt.pageX,
+          y: evt.pageY,
+        };
+      }
+    },
+    stopScroll() {
+      this.scrolling = false;
+    },
+    stopPropagate(e) {
+      e.stopPropagation();
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import './styles/colors.scss';
+
+.card {
+  overflow: hidden;
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+  background: $lavender;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  transform-origin: 50% 50%;
+  user-select: none;
+  box-shadow: 0 30px 150px -30px rgba(0,0,0,.3);
+  transition: height .25s ease;
+  &.inactive {
+    height: 300px;
+    box-shadow: 0 10px 50px -20px rgba(0,0,0,.3);
+  }
+  &.hidden {
+    height: 0;
+  }
+  .security-name {
+    display: block;
+    padding-top: 100px;
+    font-size: 120px;
+    color: $imperial;
+    &.inactive {
+      padding-top: 0;
+    }
+  }
+  .rim-out {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 700px;
+    height: 700px;
+    min-height: 700px;
+    margin: auto;
+    margin-top: 40px;
+    left: calc(50% - 350px);
+    bottom: 500px;
+    border-radius: 50%;
+    background: $thistle;
+    box-shadow: 0 10px 50px rgba(0,0,0,.15) inset;
+  }
+  .rim-in {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 400px;
+    height: 400px;
+    border-radius: 50%;
+    background: $lavender;
+    text-align: center;
+    box-shadow: 0 10px 50px rgba(0,0,0,.15);
+  }
+  .data {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding-top: 15px;
+    .label {
+      font-size: 56px;
+    }
+    .value {
+      font-size: 130px;
+    }
+  }
+  .swipe-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 100px;
+    .swipe-up {
+      width: 100px;
+      height: 150px;
+      .line-group {
+        transform: translate(50px, 10px);
+        .swipe-line {
+          fill: none;
+          stroke: $pearl;
+          stroke-width: 10px;
+          stroke-linecap: round;
+        }
+      }
+    }
+    .swipe-text {
+      padding-top: 15px;
+      font-size: 42px;
+      color: $pearl;
+    }
+  }
+}
+</style>
